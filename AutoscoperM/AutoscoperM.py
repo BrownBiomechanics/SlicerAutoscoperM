BrownBiomechanics/SlicerAutoscoperM

commit e036c2249dc564c2cca03914837248dae6d06666

ENH: Add support for passing multi-volumes through segmentation and VRG pipelines (#75)
Update segmentation and radiograph generation pipelines to support Sequential 3D and 4DCT.

Additionally, it also implements the following changes:
* Ensure Volumes corresponding to the generated virtual radiographs are centered on `[0.0 ,0.0 ,0.0]`
* Enable RAS to LPS flips by default
* Update Autoscoper to include vtkCamera schema describing `clipping-range`. The property is only
  used to communicate information between AutoscoperM and VirtualRadiographGeneration modules.

List of Autoscoper changes:

```
$ git shortlog 468324a92..8fe9748ab --no-merges 
Anthony Lombardi (1):
      ENH: Update schema to include cliping range

```

AutoscoperM/AutoscoperMLib/RadiographGeneration.py
@@ -200,7 +200,6 @@ def optimizeCameras(
 
     :return: Optimized cameras
     """
-    import glob
     import os
 
     if not progressCallback:
@@ -213,11 +212,11 @@ def optimizeCameras(
     cliNodes = []
     for i in range(len(cameras)):
         camera = cameras[i]
-        vrgFName = glob.glob(os.path.join(cameraDir, f"cam{camera.id}", "*.tif"))[0]
+        vrgDirName = os.path.join(cameraDir, f"cam{camera.id}")
         cliNode = slicer.cli.run(
             cliModule,
             None,
-            {"whiteRadiographFileName": vrgFName},
+            {"whiteRadiographDirName": vrgDirName},
             wait_for_completion=False,
         )
         cliNodes.append(cliNode)
@@ -231,7 +230,7 @@ def optimizeCameras(
             slicer.mrmlScene.RemoveNode(cliNode)
             raise ValueError("CLI execution failed: " + errorText)
         cameras[i].DID = float(cliNodes[i].GetOutputText())  # cliNodes[i].GetParameterAsString("dataIntensityDensity")
-        progress = ((i + 1) / len(cameras)) * 50 + 40
+        progress = ((i + 1) / len(cameras)) * 40 + 50
         progressCallback(progress)
         slicer.mrmlScene.RemoveNode(cliNodes[i])
 

VirtualRadiographGeneration/VirtualRadiographGeneration.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python-real
 
+import concurrent.futures as cf
+import glob
+import json
+import os
 import sys
 
 import vtk
@@ -97,36 +101,37 @@ def generateVRG(
     writer.Write()
 
 
-def _createVTKCamera(
-    position: list[float], focalPoint: list[float], viewUp: list[float], clippingRange: list[float], viewAngle: float
-) -> vtk.vtkCamera:
+def _createVTKCameras(cameraDir: str, radiographMainDir: str):
     """
     Generates a vtkCamera object from the given parameters
     """
-    camera = vtk.vtkCamera()
-    camera.SetPosition(position[0], position[1], position[2])
-    camera.SetFocalPoint(focalPoint[0], focalPoint[1], focalPoint[2])
-    camera.SetViewUp(viewUp[0], viewUp[1], viewUp[2])
-    camera.SetViewAngle(viewAngle)
-    camera.SetClippingRange(clippingRange[0], clippingRange[1])
-    return camera
+    cameras = {}
+    for camFName in glob.glob(os.path.join(cameraDir, "*.json")):
+        with open(camFName) as f:
+            camJSON = json.load(f)
 
+        cam = vtk.vtkCamera()
+        cam.SetPosition(camJSON["camera-position"])
+        cam.SetFocalPoint(camJSON["focal-point"])
+        cam.SetViewUp(camJSON["view-up"])
+        cam.SetViewAngle(camJSON["view-angle"])
+        cam.SetClippingRange(camJSON["clipping-range"])
 
-def _strToFloatList(strList: str) -> list[float]:
-    """
-    Converts a string of floats to a list of floats
-    """
-    return [float(x) for x in strList.split(",")]
+        cameraSubDirName = os.path.basename(camFName).split(".")[0]
+        cameraDirName = os.path.join(radiographMainDir, cameraSubDirName)
+        if not os.path.exists(cameraDirName):
+            os.mkdir(cameraDirName)
+
+        cameras[cam] = cameraDirName
+
+    return cameras
 
 
 if __name__ == "__main__":
     expected_args = [
         "inputVolumeFileName",
-        "cameraPosition",
-        "cameraFocalPoint",
-        "cameraViewUp",
-        "cameraViewAngle",
-        "clippingRange",
+        "cameraDir",
+        "radiographMainOutDir",
         "outputFileName",
         "outputWidth",
         "outputHeight",
@@ -137,17 +142,14 @@ if __name__ == "__main__":
         sys.exit(1)
 
     volumeData = sys.argv[1]
-    cameraPosition = _strToFloatList(sys.argv[2])
-    cameraFocalPoint = _strToFloatList(sys.argv[3])
-    cameraViewUp = _strToFloatList(sys.argv[4])
-    cameraViewAngle = float(sys.argv[5])
-    clippingRange = _strToFloatList(sys.argv[6])
-    outputFileName = sys.argv[7]
-    outputWidth = int(sys.argv[8])
-    outputHeight = int(sys.argv[9])
+    cameraDir = sys.argv[2]
+    radiographMainOutDir = sys.argv[3]
+    outputFileName = sys.argv[4]
+    outputWidth = int(sys.argv[5])
+    outputHeight = int(sys.argv[6])
 
     # create the camera
-    camera = _createVTKCamera(cameraPosition, cameraFocalPoint, cameraViewUp, clippingRange, cameraViewAngle)
+    cameras = _createVTKCameras(cameraDir, radiographMainOutDir)
 
     # Read the mhd file
     reader = vtk.vtkMetaImageReader()
@@ -155,4 +157,12 @@ if __name__ == "__main__":
     reader.Update()
 
     # generate the virtual radiograph
-    generateVRG(camera, reader.GetOutput(), outputFileName, outputWidth, outputHeight)
+    with cf.ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(
+                generateVRG, cam, reader.GetOutput(), os.path.join(camDir, outputFileName), outputWidth, outputHeight
+            )
+            for cam, camDir in cameras.items()
+        ]
+        for future in cf.as_completed(futures):
+            future.result()

AutoscoperM/AutoscoperMLib/IO.py
@@ -52,6 +52,9 @@ def generateCameraCalibrationFile(camera: Camera, filename: str):
     contents["view-angle"] = camera.vtkCamera.GetViewAngle()
     contents["image-width"] = camera.imageSize[0]
     contents["image-height"] = camera.imageSize[1]
+    # The clipping-range field is not used by Autoscoper, it is used to communicate
+    # information between AutoscoperM and VirtualRadiographGeneration modules within Slicer.
+    contents["clipping-range"] = camera.vtkCamera.GetClippingRange()
 
     contents_json = json.dumps(contents, indent=4)
 

AutoscoperM/AutoscoperMLib/SubVolumeExtraction.py
@@ -246,9 +246,8 @@ def _getItemFromFolder(folderName: str) -> slicer.vtkMRMLNode:
     shNode = slicer.vtkMRMLSubjectHierarchyNode.GetSubjectHierarchyNode(slicer.mrmlScene)
     folderId = shNode.GetItemByName(folderName)
     nodeId = shNode.GetItemByPositionUnderParent(folderId, 0)
-    nodeName = shNode.GetItemName(nodeId)
 
     folderPlugin.setDisplayVisibility(folderId, 1)
-    slicer.mrmlScene.RemoveNode(slicer.util.getNode(folderName))  # remove the folder
+    slicer.mrmlScene.RemoveNode(shNode.GetDisplayNodeForItem(folderId))  # remove the folder
 
-    return slicer.util.getNode(nodeName)  # return the node in the folder
+    return shNode.GetItemDataNode(nodeId)  # return the node in the folder

AutoscoperM/Resources/UI/AutoscoperM.ui
@@ -151,6 +151,7 @@
             <property name="nodeTypes">
              <stringlist notr="true">
               <string>vtkMRMLScalarVolumeNode</string>
+              <string>vtkMRMLSequenceNode</string>
              </stringlist>
             </property>
             <property name="hideChildNodeTypes">
@@ -210,13 +211,17 @@
              <bool>true</bool>
             </property>
             <layout class="QGridLayout" name="gridLayout_3">
-             <item row="5" column="2">
-              <widget class="QSpinBox" name="vrgRes_width">
-               <property name="maximum">
-                <number>999999999</number>
+             <item row="3" column="0">
+              <widget class="QLabel" name="label_14">
+               <property name="text">
+                <string>Camera Subdirectory:</string>
                </property>
-               <property name="value">
-                <number>1760</number>
+              </widget>
+             </item>
+             <item row="1" column="0">
+              <widget class="QLabel" name="label_20">
+               <property name="text">
+                <string>Partial Volume Transforms Subdirectory:</string>
                </property>
               </widget>
              </item>
@@ -227,27 +232,6 @@
                </property>
               </widget>
              </item>
-             <item row="6" column="2" colspan="2">
-              <widget class="QLineEdit" name="vrgTempDir">
-               <property name="text">
-                <string>VRG-Temp</string>
-               </property>
-              </widget>
-             </item>
-             <item row="1" column="2" colspan="2">
-              <widget class="QLineEdit" name="tfmSubDir">
-               <property name="text">
-                <string>Transforms</string>
-               </property>
-              </widget>
-             </item>
-             <item row="3" column="2" colspan="2">
-              <widget class="QLineEdit" name="cameraSubDir">
-               <property name="text">
-                <string>Calibration</string>
-               </property>
-              </widget>
-             </item>
              <item row="0" column="2" colspan="2">
               <widget class="QLineEdit" name="tiffSubDir">
                <property name="text">
@@ -255,10 +239,17 @@
                </property>
               </widget>
              </item>
-             <item row="6" column="0">
-              <widget class="QLabel" name="label_16">
+             <item row="5" column="0">
+              <widget class="QLabel" name="label_15">
                <property name="text">
-                <string>VRG Temp Subdirectory:</string>
+                <string>VRG Resolution: (width,height)</string>
+               </property>
+              </widget>
+             </item>
+             <item row="6" column="2" colspan="2">
+              <widget class="QLineEdit" name="vrgTempDir">
+               <property name="text">
+                <string>VRG-Temp</string>
                </property>
               </widget>
              </item>
@@ -266,13 +257,6 @@
               <widget class="QLineEdit" name="vrgSubDir">
                <property name="text">
                 <string>RadiographImages</string>
-               </property>
-              </widget>
-             </item>
-             <item row="5" column="0">
-              <widget class="QLabel" name="label_15">
-               <property name="text">
-                <string>VRG Resolution: (width,height)</string>
                </property>
               </widget>
              </item>
@@ -292,17 +276,20 @@
                </property>
               </widget>
              </item>
-             <item row="0" column="0">
-              <widget class="QLabel" name="label_12">
+             <item row="6" column="0">
+              <widget class="QLabel" name="label_16">
                <property name="text">
-                <string>Partial Volume Subdirectory:</string>
+                <string>VRG Temp Subdirectory:</string>
                </property>
               </widget>
              </item>
-             <item row="1" column="0">
-              <widget class="QLabel" name="label_20">
+             <item row="3" column="2" colspan="2">
+              <widget class="QLineEdit" name="cameraSubDir">
+               <property name="enabled">
+                <bool>false</bool>
+               </property>
                <property name="text">
-                <string>Partial Volume Transforms Subdirectory:</string>
+                <string>Calibration</string>
                </property>
               </widget>
              </item>
@@ -316,10 +303,27 @@
                </property>
               </widget>
              </item>
-             <item row="3" column="0">
-              <widget class="QLabel" name="label_14">
+             <item row="0" column="0">
+              <widget class="QLabel" name="label_12">
                <property name="text">
-                <string>Camera Subdirectory:</string>
+                <string>Partial Volume Subdirectory:</string>
+               </property>
+              </widget>
+             </item>
+             <item row="5" column="2">
+              <widget class="QSpinBox" name="vrgRes_width">
+               <property name="maximum">
+                <number>999999999</number>
+               </property>
+               <property name="value">
+                <number>1760</number>
+               </property>
+              </widget>
+             </item>
+             <item row="1" column="2" colspan="2">
+              <widget class="QLineEdit" name="tfmSubDir">
+               <property name="text">
+                <string>Transforms</string>
                </property>
               </widget>
              </item>
@@ -330,6 +334,13 @@
                </property>
                <property name="checked">
                 <bool>false</bool>
+               </property>
+              </widget>
+             </item>
+             <item row="9" column="0">
+              <widget class="QCheckBox" name="idxOnly">
+               <property name="text">
+                <string>Only use indices for radiograph filename</string>
                </property>
               </widget>
              </item>
@@ -715,6 +726,9 @@
             <property name="text">
              <string>Flip Y</string>
             </property>
+            <property name="checked">
+             <bool>true</bool>
+            </property>
            </widget>
           </item>
           <item row="0" column="3">
@@ -748,6 +762,9 @@
            <widget class="QCheckBox" name="flipX">
             <property name="text">
              <string>Flip X</string>
+            </property>
+            <property name="checked">
+             <bool>true</bool>
             </property>
            </widget>
           </item>

CalculateDataIntensityDensity/CalculateDataIntensityDensity.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python-real
 
+import concurrent.futures as cf
+import glob
 import os
 import sys
 
+import numpy as np
+import SimpleITK as sitk
 
-def main(whiteRadiographFName: str) -> float:
-    """
-    Calculates the data intensity density of the given camera on its corresponding white radiograph.
-    Internal function used by :func:`optimizeCameras`.
 
-    :param whiteRadiographFName: White radiograph file name
-
-    return Data intensity density
-    """
-
-    import numpy as np
-    import SimpleITK as sitk
-
-    MEAN_COMPARISON = 170  # 255 / 3 * 2
-
+def calcDID(whiteRadiographFName):
+    MEAN_COMPARISON = 185
     # Read in the white radiograph
-    if not isinstance(whiteRadiographFName, str):
-        raise TypeError(f"whiteRadiographFName must be a string, not {type(whiteRadiographFName)}")
-    if not os.path.isfile(whiteRadiographFName):
-        raise FileNotFoundError(f"File {whiteRadiographFName} not found.")
     whiteRadiograph = sitk.ReadImage(whiteRadiographFName)
 
     # Superpixel Segmentation
     slicImageFilter = sitk.SLICImageFilter()
-    slicImageFilter.SetSuperGridSize([15, 15, 15])  # smaller grid size = finer grid overall default is [50,50,50]
+    slicImageFilter.SetSuperGridSize([85, 85, 85])
     labelImage = slicImageFilter.Execute(whiteRadiograph)
 
     # Get the mean pixel value for each label
@@ -64,6 +52,32 @@ def main(whiteRadiographFName: str) -> float:
     largest = sortedComponentImage == 1
 
     return np.sum(sitk.GetArrayFromImage(largest))
+
+
+def main(whiteRadiographDirName: str) -> float:
+    """
+    Calculates the data intensity density of the given camera on its corresponding white radiograph.
+    Internal function used by :func:`optimizeCameras`.
+
+    :param whiteRadiographFName: White radiograph file name
+
+    return Data intensity density
+    """
+    whiteRadiographFiles = glob.glob(os.path.join(whiteRadiographDirName, "*.tif"))
+
+    if not isinstance(whiteRadiographDirName, str):
+        raise TypeError(f"whiteRadiographDirName must be a string, not {type(whiteRadiographDirName)}")
+    if not os.path.isdir(whiteRadiographDirName):
+        raise FileNotFoundError(f"Directory {whiteRadiographDirName} not found.")
+    if len(whiteRadiographFiles) == 0:
+        raise FileNotFoundError(f"No white radiographs found in {whiteRadiographDirName}")
+
+    dids = []
+    with cf.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(calcDID, wrFName) for wrFName in whiteRadiographFiles]
+        for future in cf.as_completed(futures):
+            dids.append(future.result())
+    return np.mean(dids)
 
 
 if __name__ == "__main__":

CalculateDataIntensityDensity/CalculateDataIntensityDensity.xml
@@ -15,10 +15,10 @@
   <acknowledgements><![CDATA[The SlicerAutoscoperM developers gratefully acknowledge funding for this project provided by NIH NIAMS R01AR078924 (Multi-modal Tracking of In Vivo Skeletal Structures and Implants).]]></acknowledgements>
   <parameters>
     <string>
-      <name>whiteRadiographFileName</name>
-      <label>White Radiograph File Name</label>
+      <name>whiteRadiographDirName</name>
+      <label>White Radiograph Directory Name</label>
       <index>1</index>
-      <description><![CDATA[Path to the white radiograph used to calculate data intensity density]]></description>
+      <description><![CDATA[Path to the directory that stores the white radiographs for the current camera, used to calculate data intensity density]]></description>
       <channel>input</channel>
     </string>
     <float>
@@ -26,6 +26,13 @@
       <label>Data Intensity Density (DID)</label>
       <index>2</index>
       <description><![CDATA[Calculated data intensity density]]></description>
+      <channel>output</channel>
+    </float>
+    <float>
+      <name>outliers</name>
+      <label>DID Outliers</label>
+      <index>3</index>
+      <description><![CDATA[List of indices that fall outside of the interquartile range]]></description>
       <channel>output</channel>
     </float>
     <label>IO</label>

commit 1e87e54d110b982335ff8493040351de083000d1

BUG: Fix 4DCT vrg pipeline not properly centering sequences
Regression introduced in e036c2249dc564c2cca03914837248dae6d06666

AutoscoperM/AutoscoperM.py
@@ -502,42 +502,52 @@ class AutoscoperMWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
             self.logic.createPathsIfNotExists(os.path.join(mainOutputDir, tfmPath))
             if not self.logic.IsSequenceVolume(volumeNode):
                 volumeNode.AddCenteringTransform()
-                tfmNode = slicer.util.getNode(f"{volumeNode.GetName()} centering transform")
-                volumeNode.HardenTransform()
-                volumeNode.SetAndObserveTransformNodeID(None)
-                tfmPath = os.path.join(mainOutputDir, tfmPath, "Origin2Dicom.tfm")
-                tfmNode.Inverse()
-                slicer.util.saveNode(tfmNode, tfmPath)
-                slicer.mrmlScene.RemoveNode(tfmNode)
+                tfmNode = volumeNode.GetParentTransformNode()
+                if tfmNode is not None:
+                    volumeNode.HardenTransform()
+                    volumeNode.SetAndObserveTransformNodeID(None)
+                    tfmPath = os.path.join(mainOutputDir, tfmPath, "Origin2Dicom.tfm")
+                    tfmNode.Inverse()
+                    slicer.util.saveNode(tfmNode, tfmPath)
             else:
                 # Just export the first frame
-                currentNode, _ = self.logic.getItemInSequence(volumeNode, 0)
-                currentNode.AddCenteringTransform()
-                tfmNode = currentNode.GetParentTransformNode()
-                currentNode.HardenTransform()
-                currentNode.SetAndObserveTransformNodeID(None)
-                tfmPath = os.path.join(mainOutputDir, tfmPath, "Origin2Dicom.tfm")
-                tfmNode.Inverse()
-                slicer.util.saveNode(tfmNode, tfmPath)
-                slicer.mrmlScene.RemoveNode(tfmNode)
+                proxyNode = self.logic.getItemInSequence(volumeNode, 0)[0]
+                proxyNode.AddCenteringTransform()
+                tfmNode = proxyNode.GetParentTransformNode()
 
-                # Harden and remove the transform from the sequence
-                for i in range(1, volumeNode.GetNumberOfDataNodes()):
-                    currentNode, _ = self.logic.getItemInSequence(volumeNode, i)
+                if tfmNode is not None:
+                    proxyNode.SetAndObserveTransformNodeID(None)
+                    tfmPath = os.path.join(mainOutputDir, tfmPath, "Origin2Dicom.tfm")
+                    tfmNode.Inverse()
+                    slicer.util.saveNode(tfmNode, tfmPath)
+                    tfmNode.Inverse()
+                    slicer.mrmlScene.RemoveNode(tfmNode)
+                    tfmNode = None
+
+                    currentNode = volumeNode.GetNthDataNode(0)
                     currentNode.AddCenteringTransform()
                     tfmNode = currentNode.GetParentTransformNode()
                     currentNode.HardenTransform()
                     currentNode.SetAndObserveTransformNodeID(None)
-                    slicer.mrmlScene.RemoveNode(tfmNode)
+                    volumeNode.SetDataNodeAtValue(currentNode, "0")
+
+                    # Apply to all frames
+                    for i in range(1, volumeNode.GetNumberOfDataNodes()):
+                        currentNode = volumeNode.GetNthDataNode(i)
+                        currentNode.SetAndObserveTransformNodeID(tfmNode.GetID())
+                        currentNode.HardenTransform()
+                        currentNode.SetAndObserveTransformNodeID(None)
+                        volumeNode.SetDataNodeAtValue(currentNode, f"{i}")
 
             numFrames = 1
             currentNode = volumeNode
             curName = volumeNode.GetName()
             if self.logic.IsSequenceVolume(currentNode):
                 numFrames = volumeNode.GetNumberOfDataNodes()
-                currentNode, curName = self.logic.getItemInSequence(volumeNode, 0)
+                currentNode = volumeNode.GetNthDataNode(0)
+                curName = currentNode.GetName()
             bounds = [0] * 6
-            currentNode.GetBounds(bounds)
+            currentNode.GetRASBounds(bounds)
 
             # Generate all possible camera positions
             camOffset = self.ui.camOffSetSpin.value
@@ -570,7 +580,8 @@ class AutoscoperMWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
                 self.updateProgressBar(progress)
 
                 if self.logic.IsSequenceVolume(volumeNode):
-                    currentNode, curName = self.logic.getNextItemInSequence(volumeNode)
+                    currentNode = volumeNode.GetNthDataNode(i)
+                    curName = currentNode.GetName()
 
             # Optimize the camera positions
             bestCameras = RadiographGeneration.optimizeCameras(
@@ -592,15 +603,19 @@ class AutoscoperMWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
             if self.ui.removeVrgTmp.isChecked():
                 shutil.rmtree(os.path.join(mainOutputDir, tmpDir))
 
+            if not self.logic.IsSequenceVolume(volumeNode) and tfmNode is not None:
+                volumeNode.SetAndObserveTransformNodeID(tfmNode.GetID())
+                volumeNode.HardenTransform()
+            elif tfmNode is not None:
+                tfmNode.Inverse()
+                for i in range(0, volumeNode.GetNumberOfDataNodes()):
+                    currentNode = volumeNode.GetNthDataNode(i)
+                    currentNode.SetAndObserveTransformNodeID(tfmNode.GetID())
+                    currentNode.HardenTransform()
+                    currentNode.SetAndObserveTransformNodeID(None)
+                    volumeNode.SetDataNodeAtValue(currentNode, f"{i}")
+            slicer.mrmlScene.RemoveNode(tfmNode)
         slicer.util.messageBox("Success!")
-
-        if not self.logic.IsSequenceVolume(volumeNode):
-            firstNode = volumeNode
-        else:
-            firstNode, _ = self.logic.getItemInSequence(volumeNode, 0)
-        firstNode.SetAndObserveTransformNodeID(tfmNode.GetID())
-        firstNode.HardenTransform()
-        slicer.mrmlScene.RemoveNode(tfmNode)
 
     def onGenerateConfig(self):
         """
@@ -1239,16 +1254,6 @@ class AutoscoperMLogic(ScriptedLoadableModuleLogic):
         :param filename: filename of the VRG
         """
         self.createPathsIfNotExists(outputDir)
-        # Apply a thresh of 0 to the volume to remove air from the volume
-        thresholdScalarVolume = slicer.modules.thresholdscalarvolume
-        parameters = {
-            "InputVolume": volumeNode.GetID(),
-            "OutputVolume": volumeNode.GetID(),
-            "ThresholdValue": 0,
-            "ThresholdType": "Below",
-            "Lower": 0,
-        }
-        slicer.cli.runSync(thresholdScalarVolume, None, parameters)
 
         # write a temporary volume to disk
         volumeFileName = "AutoscoperM_VRG_GEN_TEMP.mhd"

AutoscoperM/AutoscoperMLib/RadiographGeneration.py
@@ -82,7 +82,7 @@ def generateNCameras(
     :return: List of cameras
     """
     # find the center of the bounds
-    center = [(bounds[0] + bounds[1]) / 2, (bounds[2] + bounds[3]) / 2, (bounds[4] + bounds[5]) / 2]
+    center = [(bounds[i + 1] + bounds[i]) / 2 for i in range(0, 6, 2)]
 
     # find the largest dimension of the bounds
     largestDimension = max([bounds[1] - bounds[0], bounds[3] - bounds[2], bounds[5] - bounds[4]])

VirtualRadiographGeneration/VirtualRadiographGeneration.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python-real
 
-import concurrent.futures as cf
 import glob
 import json
 import os
@@ -26,9 +25,18 @@ def generateVRG(
     :param height: Height of the output image
     """
 
+    # Zero threshold the image
+    thresholder = vtk.vtkImageThreshold()
+    thresholder.ThresholdByLower(0)
+    thresholder.ReplaceInOn()
+    thresholder.SetInValue(0)
+    thresholder.SetInputData(volumeImageData)
+    thresholder.Update()
+    thresholdedImageData = thresholder.GetOutput()
+
     # find the min and max scalar values
     hist = vtk.vtkImageHistogramStatistics()
-    hist.SetInputData(volumeImageData)
+    hist.SetInputData(thresholdedImageData)
     hist.Update()
     minVal = hist.GetMinimum()
     maxVal = hist.GetMaximum()
@@ -46,7 +54,7 @@ def generateVRG(
 
     # create the volume mapper
     volumeMapper = vtk.vtkGPUVolumeRayCastMapper()
-    volumeMapper.SetInputData(volumeImageData)
+    volumeMapper.SetInputData(thresholdedImageData)
     volumeMapper.SetBlendModeToComposite()
     volumeMapper.SetBlendModeToComposite()
     volumeMapper.SetUseJittering(False)
@@ -156,13 +164,5 @@ if __name__ == "__main__":
     reader.SetFileName(volumeData)
     reader.Update()
 
-    # generate the virtual radiograph
-    with cf.ThreadPoolExecutor() as executor:
-        futures = [
-            executor.submit(
-                generateVRG, cam, reader.GetOutput(), os.path.join(camDir, outputFileName), outputWidth, outputHeight
-            )
-            for cam, camDir in cameras.items()
-        ]
-        for future in cf.as_completed(futures):
-            future.result()
+    for cam, camDir in cameras.items():
+        generateVRG(cam, reader.GetOutput(), os.path.join(camDir, outputFileName), outputWidth, outputHeight)