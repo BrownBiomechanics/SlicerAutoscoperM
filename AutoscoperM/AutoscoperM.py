BrownBiomechanics/SlicerAutoscoperM

commit e036c2249dc564c2cca03914837248dae6d06666

ENH: Add support for passing multi-volumes through segmentation and VRG pipelines (#75)
Update segmentation and radiograph generation pipelines to support Sequential 3D and 4DCT.

Additionally, it also implements the following changes:
* Ensure Volumes corresponding to the generated virtual radiographs are centered on `[0.0 ,0.0 ,0.0]`
* Enable RAS to LPS flips by default
* Update Autoscoper to include vtkCamera schema describing `clipping-range`. The property is only
  used to communicate information between AutoscoperM and VirtualRadiographGeneration modules.

List of Autoscoper changes:

```
$ git shortlog 468324a92..8fe9748ab --no-merges 
Anthony Lombardi (1):
      ENH: Update schema to include cliping range

```

AutoscoperM/Resources/UI/AutoscoperM.ui
@@ -151,6 +151,7 @@
             <property name="nodeTypes">
              <stringlist notr="true">
               <string>vtkMRMLScalarVolumeNode</string>
+              <string>vtkMRMLSequenceNode</string>
              </stringlist>
             </property>
             <property name="hideChildNodeTypes">
@@ -210,13 +211,17 @@
              <bool>true</bool>
             </property>
             <layout class="QGridLayout" name="gridLayout_3">
-             <item row="5" column="2">
-              <widget class="QSpinBox" name="vrgRes_width">
-               <property name="maximum">
-                <number>999999999</number>
+             <item row="3" column="0">
+              <widget class="QLabel" name="label_14">
+               <property name="text">
+                <string>Camera Subdirectory:</string>
                </property>
-               <property name="value">
-                <number>1760</number>
+              </widget>
+             </item>
+             <item row="1" column="0">
+              <widget class="QLabel" name="label_20">
+               <property name="text">
+                <string>Partial Volume Transforms Subdirectory:</string>
                </property>
               </widget>
              </item>
@@ -227,27 +232,6 @@
                </property>
               </widget>
              </item>
-             <item row="6" column="2" colspan="2">
-              <widget class="QLineEdit" name="vrgTempDir">
-               <property name="text">
-                <string>VRG-Temp</string>
-               </property>
-              </widget>
-             </item>
-             <item row="1" column="2" colspan="2">
-              <widget class="QLineEdit" name="tfmSubDir">
-               <property name="text">
-                <string>Transforms</string>
-               </property>
-              </widget>
-             </item>
-             <item row="3" column="2" colspan="2">
-              <widget class="QLineEdit" name="cameraSubDir">
-               <property name="text">
-                <string>Calibration</string>
-               </property>
-              </widget>
-             </item>
              <item row="0" column="2" colspan="2">
               <widget class="QLineEdit" name="tiffSubDir">
                <property name="text">
@@ -255,10 +239,17 @@
                </property>
               </widget>
              </item>
-             <item row="6" column="0">
-              <widget class="QLabel" name="label_16">
+             <item row="5" column="0">
+              <widget class="QLabel" name="label_15">
                <property name="text">
-                <string>VRG Temp Subdirectory:</string>
+                <string>VRG Resolution: (width,height)</string>
+               </property>
+              </widget>
+             </item>
+             <item row="6" column="2" colspan="2">
+              <widget class="QLineEdit" name="vrgTempDir">
+               <property name="text">
+                <string>VRG-Temp</string>
                </property>
               </widget>
              </item>
@@ -266,13 +257,6 @@
               <widget class="QLineEdit" name="vrgSubDir">
                <property name="text">
                 <string>RadiographImages</string>
-               </property>
-              </widget>
-             </item>
-             <item row="5" column="0">
-              <widget class="QLabel" name="label_15">
-               <property name="text">
-                <string>VRG Resolution: (width,height)</string>
                </property>
               </widget>
              </item>
@@ -292,17 +276,20 @@
                </property>
               </widget>
              </item>
-             <item row="0" column="0">
-              <widget class="QLabel" name="label_12">
+             <item row="6" column="0">
+              <widget class="QLabel" name="label_16">
                <property name="text">
-                <string>Partial Volume Subdirectory:</string>
+                <string>VRG Temp Subdirectory:</string>
                </property>
               </widget>
              </item>
-             <item row="1" column="0">
-              <widget class="QLabel" name="label_20">
+             <item row="3" column="2" colspan="2">
+              <widget class="QLineEdit" name="cameraSubDir">
+               <property name="enabled">
+                <bool>false</bool>
+               </property>
                <property name="text">
-                <string>Partial Volume Transforms Subdirectory:</string>
+                <string>Calibration</string>
                </property>
               </widget>
              </item>
@@ -316,10 +303,27 @@
                </property>
               </widget>
              </item>
-             <item row="3" column="0">
-              <widget class="QLabel" name="label_14">
+             <item row="0" column="0">
+              <widget class="QLabel" name="label_12">
                <property name="text">
-                <string>Camera Subdirectory:</string>
+                <string>Partial Volume Subdirectory:</string>
+               </property>
+              </widget>
+             </item>
+             <item row="5" column="2">
+              <widget class="QSpinBox" name="vrgRes_width">
+               <property name="maximum">
+                <number>999999999</number>
+               </property>
+               <property name="value">
+                <number>1760</number>
+               </property>
+              </widget>
+             </item>
+             <item row="1" column="2" colspan="2">
+              <widget class="QLineEdit" name="tfmSubDir">
+               <property name="text">
+                <string>Transforms</string>
                </property>
               </widget>
              </item>
@@ -330,6 +334,13 @@
                </property>
                <property name="checked">
                 <bool>false</bool>
+               </property>
+              </widget>
+             </item>
+             <item row="9" column="0">
+              <widget class="QCheckBox" name="idxOnly">
+               <property name="text">
+                <string>Only use indices for radiograph filename</string>
                </property>
               </widget>
              </item>
@@ -715,6 +726,9 @@
             <property name="text">
              <string>Flip Y</string>
             </property>
+            <property name="checked">
+             <bool>true</bool>
+            </property>
            </widget>
           </item>
           <item row="0" column="3">
@@ -748,6 +762,9 @@
            <widget class="QCheckBox" name="flipX">
             <property name="text">
              <string>Flip X</string>
+            </property>
+            <property name="checked">
+             <bool>true</bool>
             </property>
            </widget>
           </item>

AutoscoperM/AutoscoperMLib/IO.py
@@ -52,6 +52,9 @@ def generateCameraCalibrationFile(camera: Camera, filename: str):
     contents["view-angle"] = camera.vtkCamera.GetViewAngle()
     contents["image-width"] = camera.imageSize[0]
     contents["image-height"] = camera.imageSize[1]
+    # The clipping-range field is not used by Autoscoper, it is used to communicate
+    # information between AutoscoperM and VirtualRadiographGeneration modules within Slicer.
+    contents["clipping-range"] = camera.vtkCamera.GetClippingRange()
 
     contents_json = json.dumps(contents, indent=4)
 

AutoscoperM/AutoscoperMLib/RadiographGeneration.py
@@ -200,7 +200,6 @@ def optimizeCameras(
 
     :return: Optimized cameras
     """
-    import glob
     import os
 
     if not progressCallback:
@@ -213,11 +212,11 @@ def optimizeCameras(
     cliNodes = []
     for i in range(len(cameras)):
         camera = cameras[i]
-        vrgFName = glob.glob(os.path.join(cameraDir, f"cam{camera.id}", "*.tif"))[0]
+        vrgDirName = os.path.join(cameraDir, f"cam{camera.id}")
         cliNode = slicer.cli.run(
             cliModule,
             None,
-            {"whiteRadiographFileName": vrgFName},
+            {"whiteRadiographDirName": vrgDirName},
             wait_for_completion=False,
         )
         cliNodes.append(cliNode)
@@ -231,7 +230,7 @@ def optimizeCameras(
             slicer.mrmlScene.RemoveNode(cliNode)
             raise ValueError("CLI execution failed: " + errorText)
         cameras[i].DID = float(cliNodes[i].GetOutputText())  # cliNodes[i].GetParameterAsString("dataIntensityDensity")
-        progress = ((i + 1) / len(cameras)) * 50 + 40
+        progress = ((i + 1) / len(cameras)) * 40 + 50
         progressCallback(progress)
         slicer.mrmlScene.RemoveNode(cliNodes[i])
 

AutoscoperM/AutoscoperMLib/SubVolumeExtraction.py
@@ -246,9 +246,8 @@ def _getItemFromFolder(folderName: str) -> slicer.vtkMRMLNode:
     shNode = slicer.vtkMRMLSubjectHierarchyNode.GetSubjectHierarchyNode(slicer.mrmlScene)
     folderId = shNode.GetItemByName(folderName)
     nodeId = shNode.GetItemByPositionUnderParent(folderId, 0)
-    nodeName = shNode.GetItemName(nodeId)
 
     folderPlugin.setDisplayVisibility(folderId, 1)
-    slicer.mrmlScene.RemoveNode(slicer.util.getNode(folderName))  # remove the folder
+    slicer.mrmlScene.RemoveNode(shNode.GetDisplayNodeForItem(folderId))  # remove the folder
 
-    return slicer.util.getNode(nodeName)  # return the node in the folder
+    return shNode.GetItemDataNode(nodeId)  # return the node in the folder

CalculateDataIntensityDensity/CalculateDataIntensityDensity.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python-real
 
+import concurrent.futures as cf
+import glob
 import os
 import sys
 
+import numpy as np
+import SimpleITK as sitk
 
-def main(whiteRadiographFName: str) -> float:
-    """
-    Calculates the data intensity density of the given camera on its corresponding white radiograph.
-    Internal function used by :func:`optimizeCameras`.
 
-    :param whiteRadiographFName: White radiograph file name
-
-    return Data intensity density
-    """
-
-    import numpy as np
-    import SimpleITK as sitk
-
-    MEAN_COMPARISON = 170  # 255 / 3 * 2
-
+def calcDID(whiteRadiographFName):
+    MEAN_COMPARISON = 185
     # Read in the white radiograph
-    if not isinstance(whiteRadiographFName, str):
-        raise TypeError(f"whiteRadiographFName must be a string, not {type(whiteRadiographFName)}")
-    if not os.path.isfile(whiteRadiographFName):
-        raise FileNotFoundError(f"File {whiteRadiographFName} not found.")
     whiteRadiograph = sitk.ReadImage(whiteRadiographFName)
 
     # Superpixel Segmentation
     slicImageFilter = sitk.SLICImageFilter()
-    slicImageFilter.SetSuperGridSize([15, 15, 15])  # smaller grid size = finer grid overall default is [50,50,50]
+    slicImageFilter.SetSuperGridSize([85, 85, 85])
     labelImage = slicImageFilter.Execute(whiteRadiograph)
 
     # Get the mean pixel value for each label
@@ -64,6 +52,32 @@ def main(whiteRadiographFName: str) -> float:
     largest = sortedComponentImage == 1
 
     return np.sum(sitk.GetArrayFromImage(largest))
+
+
+def main(whiteRadiographDirName: str) -> float:
+    """
+    Calculates the data intensity density of the given camera on its corresponding white radiograph.
+    Internal function used by :func:`optimizeCameras`.
+
+    :param whiteRadiographFName: White radiograph file name
+
+    return Data intensity density
+    """
+    whiteRadiographFiles = glob.glob(os.path.join(whiteRadiographDirName, "*.tif"))
+
+    if not isinstance(whiteRadiographDirName, str):
+        raise TypeError(f"whiteRadiographDirName must be a string, not {type(whiteRadiographDirName)}")
+    if not os.path.isdir(whiteRadiographDirName):
+        raise FileNotFoundError(f"Directory {whiteRadiographDirName} not found.")
+    if len(whiteRadiographFiles) == 0:
+        raise FileNotFoundError(f"No white radiographs found in {whiteRadiographDirName}")
+
+    dids = []
+    with cf.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(calcDID, wrFName) for wrFName in whiteRadiographFiles]
+        for future in cf.as_completed(futures):
+            dids.append(future.result())
+    return np.mean(dids)
 
 
 if __name__ == "__main__":

CalculateDataIntensityDensity/CalculateDataIntensityDensity.xml
@@ -15,10 +15,10 @@
   <acknowledgements><![CDATA[The SlicerAutoscoperM developers gratefully acknowledge funding for this project provided by NIH NIAMS R01AR078924 (Multi-modal Tracking of In Vivo Skeletal Structures and Implants).]]></acknowledgements>
   <parameters>
     <string>
-      <name>whiteRadiographFileName</name>
-      <label>White Radiograph File Name</label>
+      <name>whiteRadiographDirName</name>
+      <label>White Radiograph Directory Name</label>
       <index>1</index>
-      <description><![CDATA[Path to the white radiograph used to calculate data intensity density]]></description>
+      <description><![CDATA[Path to the directory that stores the white radiographs for the current camera, used to calculate data intensity density]]></description>
       <channel>input</channel>
     </string>
     <float>
@@ -26,6 +26,13 @@
       <label>Data Intensity Density (DID)</label>
       <index>2</index>
       <description><![CDATA[Calculated data intensity density]]></description>
+      <channel>output</channel>
+    </float>
+    <float>
+      <name>outliers</name>
+      <label>DID Outliers</label>
+      <index>3</index>
+      <description><![CDATA[List of indices that fall outside of the interquartile range]]></description>
       <channel>output</channel>
     </float>
     <label>IO</label>

SuperBuild/External_Autoscoper.cmake
@@ -35,7 +35,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${SUPERBUILD_TOPLEVEL_PROJECT}_USE_SYSTEM_${p
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "468324a92840b62653c52474ff20bc7897284bd0"
+    "8fe9748ab0680955a8c18015009eff0cb457e29a"
     QUIET
   )
 

commit b37fd41e94ffa0e8da686b595548f8a54d13a4ea

BUG: Fix partial volume flip
Regression introduced in e036c2249dc564c2cca03914837248dae6d06666 where,
on config generation, the partial volumes would be automatically flipped
in the x and y directions. This caused issues with post-processing.

AutoscoperM/AutoscoperM.py
@@ -1510,11 +1510,10 @@ class AutoscoperMLogic(ScriptedLoadableModuleLogic):
 
         slicer2autoscoper = vtk.vtkMatrix4x4()
         slicer2autoscoper.Identity()
-        # Rotation matrix for a 180 x-axis rotation
-        slicer2autoscoper.SetElement(1, 1, -slicer2autoscoper.GetElement(1, 1))
-        slicer2autoscoper.SetElement(1, 2, -slicer2autoscoper.GetElement(1, 2))
+        # Rotation matrix for a 180 y-axis rotation
+        slicer2autoscoper.SetElement(0, 0, -slicer2autoscoper.GetElement(0, 0))
         slicer2autoscoper.SetElement(2, 2, -slicer2autoscoper.GetElement(2, 2))
-        slicer2autoscoper.SetElement(0, 3, -volSize[0])  # Offset -X
+        slicer2autoscoper.SetElement(1, 3, -volSize[1])  # Offset -Y
 
         slicer2autoscoperNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLLinearTransformNode")
         slicer2autoscoperNode.SetMatrixTransformToParent(slicer2autoscoper)

AutoscoperM/Resources/UI/AutoscoperM.ui
@@ -727,7 +727,7 @@
              <string>Flip Y</string>
             </property>
             <property name="checked">
-             <bool>true</bool>
+             <bool>false</bool>
             </property>
            </widget>
           </item>
@@ -764,7 +764,7 @@
              <string>Flip X</string>
             </property>
             <property name="checked">
-             <bool>true</bool>
+             <bool>false</bool>
             </property>
            </widget>
           </item>